26fb8a60e9fcd9c6e188da3d053545e5f23356ca
Harry <harryaberg@gmail.com>
2020-01-13 00:02:53 +0000

    added value table class

--- Policy-Iteration.py
+++ Policy-Iteration.py
@@ -6,11 +6,20 @@ from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter()
 
-env = gym.make('CartPole-v0')
 
 value = NN([np.prod(env.observation_space.shape), 16, 16, 1], distribution=False)
 
+# policy iteration needs a value table to perform the argmax over actions
+class ValueTable():
+    def __init___(self, grid_size):
+        self.table = np.zeros((grid_size, grid_size))
+
+    def update_value_table():
+        #  update value table at point in table
+
 def train():
+    env = gym.make('CartPole-v0')
+    value_table = create_value_table(env)
     for policy_idx in range(num_updates): # every update creates a new policy
         # RUN EPISODES
         for episode_idx in range(episodes_per_update):
@@ -18,7 +27,7 @@ def train():
             state = env.reset()
             while not done:
                 state = torch.tensor(state)
-                                
+
                 for a in range(env.action_space.n): # cant resample action spaces because you'd have to revisit them
 
                     advantage = reward
